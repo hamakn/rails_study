e0bc2c414b23dc45e1ea7fb3f76ab2e6f01da1cc
Katsunori Kawaguchi <hamakata@gmail.com>
2011-10-20 19:47:03 +0900

    create first spec about Salary#tax

--- spec/models/salary_spec.rb
+++ spec/models/salary_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Salary do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#tax" do
+    before do
+      @salary = Salary.new(:fee => 600000, :published_at => Time.parse("2011-10-20 00:00:00"))
+    end
+    it "税金は給料の10%であること" do
+      @salary.tax.should equal(60000)
+    end
+  end
 end
