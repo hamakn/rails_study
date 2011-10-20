cd39b51543d6d19db5889fd8f91835101ebf4756
Katsunori Kawaguchi <hamakata@gmail.com>
2011-10-20 19:26:33 +0900

    rails new salary_management --skip-bundle -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalaryManagement::Application.config.secret_token = '181812121f289e6a7542c15256e742041a53762a12d9342307116d285ba47b96869c01f6d6cb9406e965dbb80e6251313e80eeadbf24b4f4017c3f2ea3988d23'
